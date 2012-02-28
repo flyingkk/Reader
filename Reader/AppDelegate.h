e80d2a2cf9edef89a031eda5bf2ebc0a4ca38ee0
flyingkk <yuhuakuang@gmail.com>
2012-02-28 11:05:22 +0800

    second commit

---
+++ Reader/AppDelegate.h
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.h
+//  Reader
+//
+//  Created by user on 12-2-28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>{
+    IBOutlet UIWindow *window;
+    IBOutlet UINavigationController *navigationController;
+}
+
+@property (strong, nonatomic) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Reader/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  Reader
+//
+//  Created by user on 12-2-28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
